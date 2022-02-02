888019e8a4668373646b3a57d2aacad6436fc324
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-02-02 14:45:24 -0500

    Add logic counter

---
+++ app.js
@@ -0,0 +1,19 @@
+// create a counter app
+
+const buttonAdd = document.getElementById("add-counter");
+const number = document.getElementById("number-counter");
+
+let counter = 0;
+
+function addNumber() {
+  number.innerHTML = counter;
+  counter++;
+  console.log(counter);
+}
+
+function lessNumber() {
+  number.innerHTML = counter;
+  counter--;
+}
+
+// Este codigo funciona pero puede ser mejor intentalo mehorar
